ad4ed1aa63f64e72daca0c45888083b2cf613d0f
Theresa Gepitulan <theresagepitulan28@gmail.com>
2025-12-10 23:58:51 +0800

    Add brick walls, screen-shake, tail-strip mechanic & const fix

--- game.cpp
+++ game.cpp
@@ -3,8 +3,65 @@
 #include "raylib.h"
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
+#include <vector>
+#include <algorithm>
 
+// ----------  WALL SYSTEM  ----------
+struct Wall {
+    int x, y;
+};
+std::vector<Wall> walls;
+float shakeTime = 0.0f;        // seconds left
+float shakeIntensity = 12.0f;  // pixels
 
+// ----------  WALL HELPERS  ----------
+void drawBrickWall(int gx, int gy, int cellSize, int offsetY) {
+    int px = gx * cellSize;
+    int py = gy * cellSize + offsetY;
+    Color mortar = { 160, 82, 45, 255 };   // brownish
+    DrawRectangle(px, py, cellSize, cellSize, mortar);
+
+    int rows = 4;
+    int rowH = cellSize / rows;
+    for (int r = 0; r < rows; ++r) {
+        bool oddRow = (r & 1);
+        int bricksPerRow = 3;
+        int brickW = cellSize / bricksPerRow;
+        for (int b = 0; b < bricksPerRow; ++b) {
+            int bx = px + b * brickW + (oddRow ? brickW / 2 : 0);
+            int by = py + r * rowH;
+            Color brick = { 178, 34, 34, 255 };  // red brick
+            DrawRectangle(bx, by, brickW - 2, rowH - 2, brick);
+            DrawRectangle(bx, by, brickW - 2, 2, { 200, 70, 70, 255 }); // highlight
+        }
+    }
+    DrawRectangleLines(px, py, cellSize, cellSize, DARKBROWN);
+}
+
+void spawnWalls(int level, int gridWidth, int gridHeight, const Train& train, int cargoX, int cargoY) {
+    if (level < 5) return;
+    int count = level - 4;
+    for (int i = 0; i < count; ++i) {
+        int wx, wy;
+        do {
+            wx = rand() % gridWidth;
+            wy = rand() % gridHeight;
+        } while (train.isOnPosition(wx, wy) || (wx == cargoX && wy == cargoY));
+        walls.push_back({wx, wy});
+    }
+}
+
+void stripLastCarriage(Train& train) {
+    if (train.head == train.tail) return;
+    TrainCarriage* cur = train.head;
+    while (cur->next != train.tail) cur = cur->next;
+    delete train.tail;
+    train.tail = cur;
+    train.tail->next = nullptr;
+}
+
+// ----------  GAME  ----------
 enum GameState { START_MENU, PLAYING, GAME_OVER };
 
 int main() {
@@ -14,7 +71,7 @@ int main() {
 
     const int topBarHeight = 60;
     const int cellSize = 20;
-    const int gridWidth = screenWidth / cellSize;
+    const int gridWidth  = screenWidth / cellSize;
     const int gridHeight = (screenHeight - topBarHeight) / cellSize;
 
     Train train(gridWidth / 2, gridHeight / 2);
@@ -22,7 +79,7 @@ int main() {
     int dirX = 1;
     int dirY = 0;
 
-    srand(time(nullptr));
+    srand(static_cast<unsigned>(time(nullptr)));
     int cargoX = rand() % gridWidth;
     int cargoY = rand() % gridHeight;
 
@@ -33,184 +90,163 @@ int main() {
     GameState gameState = START_MENU;
 
     float blinkTimer = 0.0f;
-    bool showPrompt = true;
+    bool showPrompt   = true;
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         float deltaTime = GetFrameTime();
-        float time = GetTime();
+        float time      = GetTime();
 
         switch (gameState) {
-            case START_MENU: {
-                blinkTimer += deltaTime;
-                if (blinkTimer >= 0.5f) {
-                    showPrompt = !showPrompt;
-                    blinkTimer = 0.0f;
-                }
+        case START_MENU: {
+            blinkTimer += deltaTime;
+            if (blinkTimer >= 0.5f) { showPrompt = !showPrompt; blinkTimer = 0.0f; }
 
-                BeginDrawing();
-                ClearBackground((Color){ 135, 206, 235, 255 }); // Sky Blue
+            BeginDrawing();
+            ClearBackground({135, 206, 235, 255});
+            drawHills(screenWidth, screenHeight, time);
+            drawClouds(screenWidth, screenHeight, time);
 
-                drawHills(screenWidth, screenHeight, time);
-                drawClouds(screenWidth, screenHeight, time);
+            static float trainX = 0.0f;
+            static float trainSpeed = 60.0f;
+            trainX += trainSpeed * deltaTime;
+            if (trainX > GetScreenWidth() || trainX < -200) trainSpeed *= -1.0f;
 
-                static float trainX = 0;
-                static float trainSpeed = 60.0f;
-                trainX += trainSpeed * deltaTime;
-                if (trainX > GetScreenWidth() || trainX < -200) trainSpeed *= -1;
+            DrawRectangleRounded({trainX, float(GetScreenHeight() - 100), 200, 60}, 0.2f, 6, DARKBLUE);
+            DrawCircle(int(trainX + 30), GetScreenHeight() - 40, 15, BLACK);
+            DrawCircle(int(trainX + 70), GetScreenHeight() - 40, 15, BLACK);
+            DrawCircle(int(trainX + 150), GetScreenHeight() - 40, 15, BLACK);
 
-                DrawRectangleRounded((Rectangle){ (float)trainX, (float)(GetScreenHeight() - 100), 200.0f, 60.0f }, 0.2f, 6, DARKBLUE);
-                DrawCircle((int)(trainX + 30), GetScreenHeight() - 40, 20, Fade(BLACK, 0.3f));
-                DrawCircle((int)(trainX + 70), GetScreenHeight() - 40, 20, Fade(BLACK, 0.3f));
-                DrawCircle((int)(trainX + 150), GetScreenHeight() - 40, 20, Fade(BLACK, 0.3f));
+            DrawText("TRAIN ADVENTURE", GetScreenWidth()/2 - MeasureText("TRAIN ADVENTURE", 40)/2, 120, 40, DARKBLUE);
+            if (showPrompt) DrawText("Press ENTER to Start", GetScreenWidth()/2 - MeasureText("Press ENTER to Start", 20)/2, 200, 20, DARKGRAY);
+            EndDrawing();
 
-                DrawCircle((int)(trainX + 30), GetScreenHeight() - 40, 15, BLACK);
-                DrawCircle((int)(trainX + 70), GetScreenHeight() - 40, 15, BLACK);
-                DrawCircle((int)(trainX + 150), GetScreenHeight() - 40, 15, BLACK);
-
-                DrawCircleGradient((int)(trainX + 180), GetScreenHeight() - 100, 25, Fade(LIGHTGRAY, 0.6f), Fade(WHITE, 0));
-                DrawCircleGradient((int)(trainX + 210), GetScreenHeight() - 130, 18, Fade(LIGHTGRAY, 0.4f), Fade(WHITE, 0));
-                DrawCircleGradient((int)(trainX + 230), GetScreenHeight() - 160, 12, Fade(LIGHTGRAY, 0.2f), Fade(WHITE, 0));
-
-                DrawText("TRAIN ADVENTURE", GetScreenWidth() / 2 - MeasureText("TRAIN ADVENTURE", 40) / 2 + 2, 122, 40, Fade(DARKBLUE, 0.6f));
-                DrawText("TRAIN ADVENTURE", GetScreenWidth() / 2 - MeasureText("TRAIN ADVENTURE", 40) / 2, 120, 40, LIGHTGRAY);
-
-                if (showPrompt) {
-                    DrawText("PRESS ENTER TO START", GetScreenWidth() / 2 - MeasureText("PRESS ENTER TO START", 20) / 2, 300, 20, Fade(WHITE, 0.5f));
-                }
-                EndDrawing();
-
-                if (IsKeyPressed(KEY_ENTER)) {
-                    gameState = PLAYING;
-                    train = Train(gridWidth / 2, gridHeight / 2);
-                    dirX = 1;
-                    dirY = 0;
-                    level = 1;
-                    speed = 5.0f;
-                    timer = 0.0f;
-                    cargoX = rand() % gridWidth;
-                    cargoY = rand() % gridHeight;
-                }
-                break;
+            if (IsKeyPressed(KEY_ENTER)) {
+                gameState = PLAYING;
+                train       = Train(gridWidth / 2, gridHeight / 2);
+                dirX = 1; dirY = 0;
+                cargoX = rand() % gridWidth;
+                cargoY = rand() % gridHeight;
+                level  = 1;
+                speed  = 5.0f;
+                timer  = 0.0f;
+                walls.clear();
             }
-
-                        case PLAYING: {
-                timer += deltaTime;
-
-                if (IsKeyPressed(KEY_RIGHT) && !(dirX == -1 && dirY == 0)) {
-                    dirX = 1; dirY = 0;
-                } else if (IsKeyPressed(KEY_LEFT) && !(dirX == 1 && dirY == 0)) {
-                    dirX = -1; dirY = 0;
-                } else if (IsKeyPressed(KEY_UP) && !(dirX == 0 && dirY == 1)) {
-                    dirX = 0; dirY = -1;
-                } else if (IsKeyPressed(KEY_DOWN) && !(dirX == 0 && dirY == -1)) {
-                    dirX = 0; dirY = 1;
-                }
-
-                if (timer >= 1.0f / speed) {
-                    timer = 0;
-
-                    int newX = train.head->x + dirX;
-                    int newY = train.head->y + dirY;
-
-                    if (newX < 0) newX = gridWidth - 1;
-                    else if (newX >= gridWidth) newX = 0;
-
-                    if (newY < 0) newY = gridHeight - 1;
-                    else if (newY >= gridHeight) newY = 0;
-
-                    if (train.isOnPosition(newX, newY)) {
-                        gameState = GAME_OVER;
-                    } else {
-                        train.move(newX, newY);
-
-                        if (newX == cargoX && newY == cargoY) {
-                            train.addCarriage();
-
-                            int trainLength = 0;
-                            for (TrainCarriage* cur = train.head; cur != nullptr; cur = cur->next) {
-                                trainLength++;
-                            }
-
-                            if (trainLength % 3 == 0) {
-                                level++;
-                                speed = (speed + 0.5f > 15.0f) ? 15.0f : speed + 0.5f;
-                            }
-
-                            if (!placeCargo(cargoX, cargoY, train, gridWidth, gridHeight)) {
-                                gameState = GAME_OVER;
-                            }
-                        }
-                    }
-                }
-
-                BeginDrawing();
-                ClearBackground((Color){ 135, 206, 235, 255 });
-
-                drawHills(screenWidth, screenHeight, time);
-                drawClouds(screenWidth, screenHeight, time);
-
-                /* =====  TOP-BAR UI  ===== */
-                int trainLength = 0;
-                for (TrainCarriage* cur = train.head; cur != nullptr; cur = cur->next) {
-                    trainLength++;
-                }
-
-                // Level box
-                Rectangle levelBox = {10, 10, 120, 40};
-                DrawRectangleRounded(levelBox, 0.2f, 6, BLUE);
-                DrawRectangleRoundedLines(levelBox, 0.2f, 6, DARKBLUE);
-                DrawText("Level", levelBox.x + 15, levelBox.y + 5, 18, WHITE);
-                DrawText(TextFormat("%d", level), levelBox.x + 15, levelBox.y + 22, 22, YELLOW);
-
-                // Length box
-                Rectangle lengthBox = {140, 10, 140, 40};
-                DrawRectangleRounded(lengthBox, 0.2f, 6, BLUE);
-                DrawRectangleRoundedLines(lengthBox, 0.2f, 6, DARKBLUE);
-                DrawText("Length", lengthBox.x + 15, lengthBox.y + 5, 18, WHITE);
-                DrawText(TextFormat("%d", trainLength), lengthBox.x + 15, lengthBox.y + 22, 22, YELLOW);
-
-                // Instruction box
-                Rectangle instrBox = {300, 10, screenWidth - 310, 40};
-                DrawRectangleRounded(instrBox, 0.2f, 6, GRAY);
-                DrawRectangleRoundedLines(instrBox, 0.2f, 6, DARKGRAY);
-                DrawText("Use arrow keys to drive the train", instrBox.x + 15, instrBox.y + 12, 20, BLACK);
-
-                /* =====  GAME WORLD  ===== */
-                drawTrainTracks(gridWidth, gridHeight, cellSize, topBarHeight);
-                drawCargo(cargoX, cargoY, cellSize, topBarHeight);
-                drawTrain(train, cellSize, topBarHeight, dirX, dirY);
-
-                EndDrawing();
-                break;
-            }
-
-            case GAME_OVER: {
-                blinkTimer += deltaTime;
-                if (blinkTimer >= 0.5f) {
-                    showPrompt = !showPrompt;
-                    blinkTimer = 0.0f;
-                }
-
-                BeginDrawing();
-                ClearBackground((Color){ 20, 20, 20, 255 });
-                DrawText("GAME OVER", screenWidth / 2 - MeasureText("GAME OVER", 40) / 2, 120, 40, RED);
-                DrawText(TextFormat("YOUR SCORE: %d", level), screenWidth / 2 - MeasureText("YOUR SCORE: 00", 30) / 2, 200, 30, WHITE);
-
-                if (showPrompt) {
-                    DrawText("PRESS ENTER TO RESTART", screenWidth / 2 - MeasureText("PRESS ENTER TO RESTART", 20) / 2, 300, 20, Fade(WHITE, 0.5f));
-                }
-
-                EndDrawing();
-
-                if (IsKeyPressed(KEY_ENTER)) {
-                    gameState = START_MENU;
-                }
-                break;
-            }
+            break;
         }
-    }
+
+        case PLAYING: {
+            timer += deltaTime;
+
+            if (IsKeyPressed(KEY_RIGHT) && !(dirX == -1 && dirY == 0)) { dirX = 1; dirY = 0; }
+            else if (IsKeyPressed(KEY_LEFT)  && !(dirX == 1 && dirY == 0)) { dirX = -1; dirY = 0; }
+            else if (IsKeyPressed(KEY_UP)    && !(dirX == 0 && dirY == 1)) { dirX = 0; dirY = -1; }
+            else if (IsKeyPressed(KEY_DOWN)  && !(dirX == 0 && dirY == -1)) { dirX = 0; dirY = 1; }
+
+            if (timer >= 1.0f / speed) {
+                timer = 0.0f;
+
+                int newX = (train.head->x + dirX + gridWidth)  % gridWidth;
+                int newY = (train.head->y + dirY + gridHeight) % gridHeight;
+
+                if (train.isOnPosition(newX, newY)) {
+                    gameState = GAME_OVER;
+                    break;
+                }
+                // ---- brick-wall collision ----
+                bool hitWall = false;
+                for (const Wall& w : walls) {
+                    if (newX == w.x && newY == w.y) { hitWall = true; break; }
+                }
+                if (hitWall) {
+                    stripLastCarriage(train);
+                    shakeTime = 0.4f;
+                }
+                train.move(newX, newY);
+
+                if (newX == cargoX && newY == cargoY) {
+                    train.addCarriage();
+                    level++;
+                    speed = fminf(speed + 0.5f, 12.0f);
+                    spawnWalls(level, gridWidth, gridHeight, train, cargoX, cargoY);
+                    placeCargo(cargoX, cargoY, train, gridWidth, gridHeight); // ignore result
+                }
+            }
+
+            // ----------  DRAW  ----------
+            BeginDrawing();
+            // camera shake
+            if (shakeTime > 0.0f) {
+                float sx = GetRandomValue(-100, 100) / 100.0f * shakeIntensity;
+                float sy = GetRandomValue(-100, 100) / 100.0f * shakeIntensity;
+                BeginMode2D((Camera2D){ {0,0}, {sx, sy}, 0.0f, 1.0f });
+                shakeTime -= deltaTime;
+            } else {
+                BeginMode2D((Camera2D){ {0,0}, {0,0}, 0.0f, 1.0f });
+            }
+
+            ClearBackground({135, 206, 235, 255});
+            drawHills(screenWidth, screenHeight, time);
+            drawClouds(screenWidth, screenHeight, time);
+
+            int trainLength = 0;
+            for (TrainCarriage* cur = train.head; cur != nullptr; cur = cur->next) trainLength++;
+
+            Rectangle levelBox  = {10, 10, 120, 40};
+            DrawRectangleRounded(levelBox, 0.2f, 6, BLUE);
+            DrawRectangleRoundedLines(levelBox, 0.2f, 6, DARKBLUE);
+            DrawText("Level", levelBox.x + 15, levelBox.y + 5, 18, WHITE);
+            DrawText(TextFormat("%d", level), levelBox.x + 15, levelBox.y + 22, 22, YELLOW);
+
+            Rectangle lengthBox = {140, 10, 140, 40};
+            DrawRectangleRounded(lengthBox, 0.2f, 6, BLUE);
+            DrawRectangleRoundedLines(lengthBox, 0.2f, 6, DARKBLUE);
+            DrawText("Length", lengthBox.x + 15, lengthBox.y + 5, 18, WHITE);
+            DrawText(TextFormat("%d", trainLength), lengthBox.x + 15, lengthBox.y + 22, 22, YELLOW);
+
+            Rectangle instrBox  = {300, 10, screenWidth - 310, 40};
+            DrawRectangleRounded(instrBox, 0.2f, 6, GRAY);
+            DrawRectangleRoundedLines(instrBox, 0.2f, 6, DARKGRAY);
+            DrawText("Use arrow keys to drive the train", instrBox.x + 15, instrBox.y + 12, 20, BLACK);
+
+            drawTrainTracks(gridWidth, gridHeight, cellSize, topBarHeight);
+            drawCargo(cargoX, cargoY, cellSize, topBarHeight);
+            for (const Wall& w : walls) drawBrickWall(w.x, w.y, cellSize, topBarHeight);
+            drawTrain(train, cellSize, topBarHeight, dirX, dirY);
+
+            EndMode2D();
+            EndDrawing();
+            break;
+        }
+
+        case GAME_OVER: {
+            BeginDrawing();
+            ClearBackground({135, 206, 235, 255});
+            drawHills(screenWidth, screenHeight, time);
+            drawClouds(screenWidth, screenHeight, time);
+
+            DrawText("GAME OVER", GetScreenWidth()/2 - MeasureText("GAME OVER", 60)/2 + 3, GetScreenHeight()/2 - 45 + 3, 60, Fade(RED, 0.6f));
+            DrawText("GAME OVER", GetScreenWidth()/2 - MeasureText("GAME OVER", 60)/2, GetScreenHeight()/2 - 45, 60, RED);
+            DrawText("Press ENTER to Restart", GetScreenWidth()/2 - MeasureText("Press ENTER to Restart", 24)/2 + 1, GetScreenHeight()/2 + 20 + 1, 24, Fade(DARKGRAY, 0.5f));
+            DrawText("Press ENTER to Restart", GetScreenWidth()/2 - MeasureText("Press ENTER to Restart", 24)/2, GetScreenHeight()/2 + 20, 24, DARKGRAY);
+            EndDrawing();
+
+            if (IsKeyPressed(KEY_ENTER)) {
+                train  = Train(gridWidth / 2, gridHeight / 2);
+                dirX   = 1; dirY = 0;
+                cargoX = rand() % gridWidth;
+                cargoY = rand() % gridHeight;
+                level  = 1;
+                speed  = 5.0f;
+                timer  = 0.0f;
+                walls.clear();
+                gameState = START_MENU;
+            }
+            break;
+        }
+        } // switch
+    }     // while window open
 
     CloseWindow();
     return 0;
